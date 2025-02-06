c09c87fae48f6ef39a77f2e5d0c3eccd675ba6d8
LucasPort <lb.lucasbourdon@gmail.com>
2025-02-06 13:04:12 +0100

    ajout de la navigation et des routes pour afficher les détails d'un docteur

--- doctomap-react/src/App.jsx
+++ doctomap-react/src/App.jsx
@@ -1,13 +1,17 @@
 import { Route, Routes, BrowserRouter } from "react-router-dom";
 import Home from "./pages/home/Home";
 import Doctor from "./pages/doctor/Doctor";
+import Id from "./pages/doctor/id";
+import Nav from "./pages/components/nav/nav";
 
 function App() {
   return (
     <BrowserRouter>
+      <Nav />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/doctors" element={<Doctor />} />
+        <Route path="/doctors/:id" element={<Id />} />
       </Routes>
     </BrowserRouter>
   );
