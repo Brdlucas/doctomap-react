0e56658384b3712478b33ec745116eb2411e847d
LucasPort <lb.lucasbourdon@gmail.com>
2025-02-06 16:13:01 +0100

    mise à jour de la page du docteur

--- doctomap-react/src/pages/doctor/Id.jsx
+++ doctomap-react/src/pages/doctor/Id.jsx
@@ -46,21 +46,16 @@ function Id() {
           </div>
           <div className="col-span-4 sm:col-span-9">
             <div className="bg-white shadow rounded-lg p-6">
-              <h2 className="text-xl font-bold mb-4">About Me</h2>
+              <h2 className="text-xl font-bold mb-4">A propos</h2>
               <p className="text-gray-700">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-                finibus est vitae tortor ullamcorper, ut vestibulum velit
-                convallis. Aenean posuere risus non velit egestas suscipit. Nunc
-                finibus vel ante id euismod. Vestibulum ante ipsum primis in
-                faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam
-                erat volutpat. Nulla vulputate pharetra tellus, in luctus risus
-                rhoncus id.
+                le cabinet du docteur(e) {doctor.lastname} est un cabinet de{" "}
+                {doctor.speciality} situé a {doctor.city}, dans le {doctor.zip}.
               </p>
               <h2 className="text-xl font-bold mt-6 mb-4">Adresse</h2>
               <div className="mb-6">
                 <div className="flex justify-between flex-wrap gap-2 w-full">
                   <span className="text-gray-700 font-bold">
-                    {doctor.address} {doctor.city} {doctor.zip}
+                    {doctor.address} {doctor.city}, {doctor.zip}
                   </span>
                 </div>
               </div>
