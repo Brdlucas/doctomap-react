5b434cd9d09bab369469e59ddf72f02d93ab31e9
LucasPort <lb.lucasbourdon@gmail.com>
2025-02-06 18:18:13 +0100

    ajout de la page vers le formulaire

--- doctomap-react/src/App.jsx
+++ doctomap-react/src/App.jsx
@@ -5,6 +5,7 @@ import Id from "./pages/doctor/id";
 import Nav from "./pages/components/nav/nav";
 import Dashboard from "./pages/admin/Dashboard";
 import AdminDoctor from "./pages/admin/adminDoctor";
+import Form from "./pages/form/Form";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path="/doctors/:id" element={<Id />} />
         <Route path="/dashboard/" element={<Dashboard />} />
         <Route path="/dashboard/users" element={<AdminDoctor />} />
+        <Route path="/formulaire" element={<Form />} />
       </Routes>
     </BrowserRouter>
   );
