85c879963c2e16d84997536c3b421dbd2eb589d8
LucasPort <lb.lucasbourdon@gmail.com>
2025-02-06 18:22:22 +0100

    modification du style du formulaire et ajout d'un placeholder pour le champ d'image

--- doctomap-react/src/pages/form/Form.jsx
+++ doctomap-react/src/pages/form/Form.jsx
@@ -46,7 +46,7 @@ function Form() {
     <form
       id="doctorForm"
       onSubmit={handleSubmit}
-      className="max-w-lg mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
+      className="max-w-lg mx-auto bg-white shadow-md rounded px-8 pt-6 pb-8 m-4"
     >
       <div className="mb-4">
         <input
@@ -119,11 +119,12 @@ function Form() {
           className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="image"
           type="text"
+          placeholder="lien de l'image"
         />
       </div>
       <div className="flex items-center justify-between">
         <button
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+          className=" bg-black cursor-pointer hover:bg-purple-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
           type="submit"
         >
           Envoyer
