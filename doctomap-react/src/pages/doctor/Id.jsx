f1fb82fc22fc0410d9ec87fc3b609e9363e6c7a1
LucasPort <lb.lucasbourdon@gmail.com>
2025-02-06 13:04:49 +0100

    ajout de la page de détails d'un docteur avec récupération des informations via l'API

---
+++ doctomap-react/src/pages/doctor/Id.jsx
@@ -0,0 +1,75 @@
+import { useEffect } from "react";
+import { useState } from "react";
+import { useParams } from "react-router";
+
+function Id() {
+  const params = useParams();
+  const [doctor, setDoctor] = useState({});
+
+  useEffect(() => {
+    getOneDoctor();
+  }, [params.id]);
+
+  const getOneDoctor = async () => {
+    const response = await fetch(
+      `https://127.0.0.1:8000/api/doctors/${params.id}`
+    );
+    const data = await response.json();
+    setDoctor(data);
+  };
+
+  return (
+    <div className="bg-gray-100 h-screen">
+      <div className="container mx-auto py-8">
+        <div className="grid grid-cols-4 sm:grid-cols-12 gap-6 px-4">
+          <div className="col-span-4 sm:col-span-3">
+            <div className="bg-white shadow rounded-lg p-6">
+              <div className="flex flex-col items-center">
+                <img
+                  src={`${doctor.image}`}
+                  className="w-32 h-32 bg-gray-300 rounded-full mb-4 shrink-0"
+                ></img>
+                <h1 className="text-xl font-bold">
+                  {doctor.firstname} {doctor.lastname}
+                </h1>
+                <p className="text-gray-700">{doctor.speciality}</p>
+                <div className="mt-6 flex flex-wrap gap-4 justify-center">
+                  <a
+                    href="#"
+                    className="bg-blue-500 hover:bg-blue-600 text-white py-2 px-4 rounded"
+                  >
+                    {doctor.phone}
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-span-4 sm:col-span-9">
+            <div className="bg-white shadow rounded-lg p-6">
+              <h2 className="text-xl font-bold mb-4">About Me</h2>
+              <p className="text-gray-700">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
+                finibus est vitae tortor ullamcorper, ut vestibulum velit
+                convallis. Aenean posuere risus non velit egestas suscipit. Nunc
+                finibus vel ante id euismod. Vestibulum ante ipsum primis in
+                faucibus orci luctus et ultrices posuere cubilia Curae; Aliquam
+                erat volutpat. Nulla vulputate pharetra tellus, in luctus risus
+                rhoncus id.
+              </p>
+              <h2 className="text-xl font-bold mt-6 mb-4">Adresse</h2>
+              <div className="mb-6">
+                <div className="flex justify-between flex-wrap gap-2 w-full">
+                  <span className="text-gray-700 font-bold">
+                    {doctor.address} {doctor.city} {doctor.zip}
+                  </span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Id;
