5ece601484b637bc8575c72da0e728980abba3aa
LucasPort <lb.lucasbourdon@gmail.com>
2025-02-06 13:04:39 +0100

    ajout de la navigation vers les détails des docteurs et mise à jour de l'affichage des informations des docteurs

--- doctomap-react/src/pages/doctor/Doctor.jsx
+++ doctomap-react/src/pages/doctor/Doctor.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router";
 
 function Doctor() {
   const [doctors, setDoctors] = useState([]);
@@ -17,20 +18,20 @@ function Doctor() {
 
   return (
     <div>
-      <h1>Doctor</h1>
-      <div className="p-5">
+      <div className="p-5 flex gap-5 flex-col">
         {doctors.map((doctor) => {
           return (
-            <section
+            <Link
+              to={`${doctor.id}`}
               key={doctor.id}
-              className="mb-2 border bg-neutral-100 p-4 rounded-lg max-w-full bg-neutral-100"
+              className="hover:scale-[101%] transition mb-2 shadow-xl border border-gray-200 bg-white p-4 rounded-lg max-w-full h-min"
             >
               <div className="mx-auto">
                 <div className="card md:flex max-w-lg">
                   <div className="w-20 h-20 mx-auto mb-6 md:mr-6 flex-shrink-0">
                     <img
                       className="object-cover rounded-full"
-                      src="https://tailwindflex.com/public/images/user.png"
+                      src={doctor.image}
                     />
                   </div>
                   <div className="flex-grow text-center md:text-left">
@@ -39,24 +40,20 @@ function Doctor() {
                       {doctor.firstname} {doctor.lastname}
                     </h3>
                     <p className="mt-2 mb-3">
-                      John is a Senior Developer, mainly works in backend
-                      technologies.
+                      Docteur(e) {doctor.lastnam} est un(e) {doctor.speciality}.
                     </p>
-                    <div>
-                      <span className="bg-gray-200 border px-3 py-1.5 rounded-lg text-sm">
-                        Discrete Math
+                    <div className="flex flex-row gap-x-3">
+                      <span className="bg-purple-700 text-white px-3 py-1.5 rounded-lg text-sm">
+                        {doctor.speciality}
                       </span>
-                      <span className="bg-gray-200 border px-3 py-1.5 rounded-lg text-sm">
-                        Topology
-                      </span>
-                      <span className="bg-gray-200 border px-3 py-1.5 rounded-lg text-sm">
-                        Neural Nets
+                      <span className="bg-purple-700 text-white border px-3 py-1.5 rounded-lg text-sm">
+                        {doctor.city}
                       </span>
                     </div>
                   </div>
                 </div>
               </div>
-            </section>
+            </Link>
           );
         })}
       </div>
